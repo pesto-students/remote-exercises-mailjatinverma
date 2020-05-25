89fb2a8e5d588cbcff06ed2e32a1a7679f5d9663
Jatin1983 <jatin@headrun.com>
2020-05-25 16:01:27 +0530

    eslint fixes

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,24 +1,25 @@
 
-function uniqueElements(...arr){
-    let unique = [...new Set(arr)];
-    return unique;
+function uniqueElements(...arr) {
+  const unique = [...new Set(arr)];
+  return unique;
 }
 
-function uniqueInFirst(arr1, arr2){
-    var uniqueInFirst = []
-    for(let i=0; i<arr1.length; i++){
-        let ch = arr1[i];
-        if ( ! arr2.includes(ch)){uniqueInFirst.push(ch);}
-    }
-    return uniqueInFirst;
+function uniqueInFirst(arr1, arr2) {
+  const uniqueInFirstArray = [];
+  for (let i = 0; i < arr1.length; i++) {
+    const ch = arr1[i];
+    if (!arr2.includes(ch)) { uniqueInFirstArray.push(ch); }
+  }
+  return uniqueInFirstArray;
 }
 
 function diffArray(arr1, arr2) {
-    let uniqueElements1 = uniqueElements(...arr1);
-    let uniqueElements2 = uniqueElements(...arr2);
+  const uniqueElements1 = uniqueElements(...arr1);
+  const uniqueElements2 = uniqueElements(...arr2);
 
-    let diffArray = uniqueInFirst(uniqueElements1, uniqueElements2).concat(uniqueInFirst(uniqueElements2, uniqueElements1));
-    return diffArray;
+  const differenceArray = uniqueInFirst(uniqueElements1, uniqueElements2)
+    .concat(uniqueInFirst(uniqueElements2, uniqueElements1));
+  return differenceArray;
 }
 
 export {
