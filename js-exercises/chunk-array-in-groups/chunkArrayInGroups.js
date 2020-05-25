26838b85112bdf2a2ef039ff522aaeb98fa6cde6
Jatin1983 <jatin@headrun.com>
2020-05-25 15:53:51 +0530

    eslint fixes

--- js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
+++ js-exercises/chunk-array-in-groups/chunkArrayInGroups.js
@@ -1,13 +1,15 @@
 function chunkArrayInGroups(array, chunkSize) {
-    var matrix = [], i, k;
-    for(i=0, k=-1; i<array.length; i++){
-        if(i%chunkSize===0){
-            k++;
-            matrix[k] = [];
-        }
-        matrix[k].push(array[i]);
+  const matrix = [];
+  let i;
+  let k;
+  for (i = 0, k = -1; i < array.length; i++) {
+    if (i % chunkSize === 0) {
+      k = k + 1;
+      matrix[k] = [];
     }
-    return matrix;
+    matrix[k].push(array[i]);
+  }
+  return matrix;
 }
 
 export {
