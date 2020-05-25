ef31f37c1f19c169dea13b38e2fd0ced27de00ad
Jatin1983 <jatin@headrun.com>
2020-05-25 16:02:12 +0530

    eslint fixes

--- js-exercises/sum-even-args/sumEvenArgs.js
+++ js-exercises/sum-even-args/sumEvenArgs.js
@@ -1,13 +1,13 @@
- const isEven = (number) => {
-    return (number % 2 === 0);
- }
+const isEven = (number) => {
+  return (number % 2 === 0);
+}
 
 const sumEvenArgs = (...args) => {
-    var sum = 0;
-    for(var index=0; index<args.length; index++)
-    {
-        if(isEven(args[index])){ sum += args[index]};
-    }
+  var sum = 0;
+  for (var index=0; index<args.length; index++)
+  {
+    if(isEven(args[index])) { sum += args[index]};
+  }
   return sum;
 };
 
